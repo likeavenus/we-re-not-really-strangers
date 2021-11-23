d4a4eec66463e394273462ac2a906c21b4afc397
Rafael <37466798+likeavenus@users.noreply.github.com>
2021-11-23 19:23:16 +0300

    master: add cards placement

--- src/containers/Game/index.jsx
+++ src/containers/Game/index.jsx
@@ -2,19 +2,40 @@ import React, { useCallback, useEffect, useState } from 'react';
 
 import styles from './style.module';
 
+const cards = [
+  {
+    id: 0,
+    type: 'love',
+    text: 'Как думаешь, я когда-нибудь сбегал из дома будучи подростком?',
+  },
+  {
+    id: 1,
+    type: 'love',
+    text: 'Как думаешь, я когда-нибудь сбегал из дома будучи подростком?',
+  },
+  {
+    id: 2,
+    type: 'love',
+    text: 'Как думаешь, я когда-нибудь сбегал из дома будучи подростком?',
+  },
+];
+
 export const Game = () => {
+  const cardsElems = cards.map((item, index) => (
+    <div key={item.id} style={{ zIndex: cards.length - item.id, transform: `scale(${(20 - index) / 20}) translateY(-${30 * index}px)` }} className={styles.card}>
+      <div className={styles.card__body}>
+        <p className={styles.card__text}>{item.text}</p>
+      </div>
+      <div className={styles.card__footer}>
+        <p className={styles.footer__text}>мы не незнакомцы</p>
+      </div>
+    </div>
+  ));
 
   return (
     <div className={styles.game}>
       <div className={styles.game__container}>
-        <div className={styles.card}>
-          <div className={styles.card__body}>
-            <p className={styles.card__text}>Как думаешь, я когда-нибудь сбегал из дома будучи подростком?</p>
-          </div>
-          <div className={styles.card__footer}>
-            <p className={styles.footer__text}>мы не незнакомцы</p>
-          </div>
-        </div>
+        {cardsElems}
       </div>
     </div>
   );

92b5dd94720865feac995313b7570965d8d589d1
Rafael <37466798+likeavenus@users.noreply.github.com>
2021-12-04 14:17:37 +0300

    master: add detection card position

--- src/containers/Game/utils.js
+++ src/containers/Game/utils.js
@@ -1,3 +1,5 @@
+const CARD_GAP = 25;
+
 export function dragStart(shift, card) {
     return function(event) {
         card.parentElement.style.backgroundColor = 'transparent';
@@ -19,21 +21,33 @@ export function dragMove(shift, card) {
         const moveY = touch.clientY - shift.y - rect.y;
 
         card.style.transform = `translate(${moveX}px, ${moveY}px) rotate(${moveX * 0.03}deg)`;
-    }
-}
 
-export function dragEnd(card) {
-    return function() {
-        const rect = card.getBoundingClientRect();
-        const CARD_GAP = 30;
+        const currentRectX = card.getBoundingClientRect().x;
         const windowWidth = window.innerWidth + CARD_GAP;
-        const triggerValue = rect.x + windowWidth / 2;
+        const triggerValue = currentRectX + windowWidth / 2;
 
         if (triggerValue > windowWidth) {
             card.parentElement.style.backgroundColor = 'lightgreen';
             
         } else if (triggerValue < 0 + CARD_GAP) {
             card.parentElement.style.backgroundColor = 'red';
+        } else {
+            card.parentElement.style.backgroundColor = 'transparent';
+        }
+    }
+}
+
+export function dragEnd(card) {
+    return function() {
+        const rect = card.getBoundingClientRect();
+        const windowWidth = window.innerWidth + CARD_GAP;
+        const triggerValue = rect.x + windowWidth / 2;
+
+        if (triggerValue > windowWidth) {
+            // card.parentElement.style.backgroundColor = 'lightgreen';
+            
+        } else if (triggerValue < 0 + CARD_GAP) {
+            // card.parentElement.style.backgroundColor = 'red';
         } else {
             card.style.transition = 'all 0.35s ease';
             card.style.transform = 'scale(1) translateY(0px)';
